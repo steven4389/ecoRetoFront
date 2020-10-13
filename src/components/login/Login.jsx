9c23d12fb93343f97900bcc167e5a042402cc11a
Carlos Ariza <ariza394@lantia.com>
2020-10-14 09:07:43 +1300

    login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+//redux
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { loginAction } from '../../actions/authAction'
 
 const Login = () => {
+
+    //redux
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const emailedux = useSelector(state => state.auth.email)
+
+    //states del componente
+    const [user,setUser] = useState({
+        email:'',
+        password:''
+    })
+
+    const [errorLogin,setError] = useState(false);
+
+    //funciones
+    const onChange = e => {
+        setUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //inicia sesion
+    const login = async e => {
+        e.preventDefault();
+        //validacion
+
+
+        //envio formulario
+        const response = await dispatch(loginAction(user));
+        //accion a tomar
+        if(response === '1'){
+            history.push('/homeApp')
+        }else if(response === '0'){
+            history.push('/profile')
+        }else if(response === 'invalid'){
+            setError(true);
+        }else{
+            //letrero error de conexion
+        }
+        
+    }
+
     return ( 
-        <h1>Hola sy login</h1>
+        <div className="home">
+            <div className="pltZoneLogin">
+                <form id="frmLogin">
+                    <div className="pltBoxLogo">
+                        {/**<img src={logo} alt="Lantia" className="pltLogo" width="180px"/> */}
+                        <span>Iniciar sesión</span>	
+                    </div>
+
+                    <fieldset>
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                id="email" 
+                                name="email" 
+                                type="text" 
+                                maxLength="45" 
+                                value={user.email}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Contraseña</label>
+                            <input 
+                                id="password" 
+                                name="password" 
+                                type="password"  
+                                value={user.password}
+                                onChange={onChange}
+                            />
+                        </div>
+
+                        <input 
+                            id="btnLogin" 
+                            type="submit" 
+                            className="untBtn darkblue" 
+                            value="INICIAR SESION"
+                            onClick={login}
+                        />
+                    </fieldset>
+                </form>                
+            </div>
+            <div className="pltLoginText">
+                <h3><span className="pltLoginSub1">Ayudamos</span> a las <br/> empresas a trabajar <br/> mas eficientemente</h3>
+                <span className="pltLoginSub2">LANTIA, plataforma integral para <br/> business intelligence, cienciometría <br/> y análisis de patentes.</span>
+            </div>
+        </div>
      );
 }
  
--- src/reducers/authreducer.js
+++ src/reducers/authreducer.js
@@ -6,11 +6,31 @@ import {
 
 //state
 const initialState = {
-    userId:null
+    email:null,
+    userId:null,
+    phone:null,
+    firstname:null,
+    lastname:null,
+    role:null,
+    autenticated:-1,
+    reload:true   
 }
 
 export default function(state = initialState, action){
     switch(action.type){
+
+        case LOGIN_SUCCESS:
+            return{
+                ...state,
+                email:action.payload.email,
+                firstname:action.payload.firstname,
+                lastname:action.payload.lastname,
+                phone:action.payload.phone,
+                role:action.payload.role,
+                userId:action.payload.userId,
+                autenticated:true,
+                reload:true
+            }
 
         default:
             return state;
